4bf6ad2a34add2a841dc0227be8a8d93b252598b
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-23 22:20:08 -0700

    Update load_data_lake.sh

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -9,7 +9,8 @@ $ mv "Hospital General Information.csv" "hospitals.csv"
 $ mv "Timely and Effective Care - Hospital.csv" "effective_care.csv"  
 $ mv "Readmissions and Deaths - Hospital.csv" "readmissions.csv"  
 $ mv "Measure Dates.csv" "measures.csv"  
-$ mv "hvbp_hcahps_05_28_2015.csv" "survey_responses.csv"  
+$ mv "hvbp_hcahps_05_28_2015.csv" "survey_responses.csv"
+$ mv "Footnote Crosswalk.csv" "footnote_crosswalk.csv"
 ## Remove headers from files  
 $ sed -i 1d *.csv  
 ## Load crucial tables into HDFS
@@ -18,4 +19,5 @@ $ hdfs dfs -put hospitals.csv /user/w205/hospital_compare
 $ hdfs dfs -put effective_care.csv /user/w205/hospital_compare  
 $ hdfs dfs -put measures.csv /user/w205/hospital_compare  
 $ hdfs dfs -put readmissions.csv /user/w205/hospital_compare  
-$ hdfs dfs -put survey_responses.csv /user/w205/hospital_compare  
+$ hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
+$ hdfs dfs -put footnote_crosswalk.csv /user/w205/hospital_compare
