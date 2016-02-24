bd310274b932fa029b2a3b99d05ca0c9bde30178
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-23 22:47:40 -0700

    Update hive_base_ddl.sql

--- loading_and_modeling/hive_base_ddl.sql
+++ loading_and_modeling/hive_base_ddl.sql
@@ -1,15 +1,15 @@
 DROP TABLE hospitals;
 CREATE EXTERNAL TABLE hospitals(pid, VARCHAR(8), 
-                                hospital_name, VARCHAR(52), 
-                                address, VARCHAR(52), 
-                                city, VARCHAR(22), 
-                                state, VARCHAR(4), 
-                                zip, VARCHAR(7), 
-                                county, VARCHAR(22), 
-                                phone, VARCHAR(12), 
-                                hospital_type, VARCHAR(38), 
-                                hospital_own, VARCHAR(45), 
-                                emergency_services, VARCHAR(5))
+                                hospital_name VARCHAR(52), 
+                                address VARCHAR(52), 
+                                city VARCHAR(22), 
+                                state VARCHAR(4), 
+                                zip VARCHAR(7), 
+                                county VARCHAR(22), 
+                                phone VARCHAR(12), 
+                                hospital_type VARCHAR(38), 
+                                hospital_own VARCHAR(45), 
+                                emergency_services VARCHAR(5))
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
