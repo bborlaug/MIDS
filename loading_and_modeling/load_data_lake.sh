0a0fb81bc79fe4500a67da6b65bb371d59c09d1e
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-25 00:53:37 -0700

    Update load_data_lake.sh

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -14,10 +14,16 @@ $ mv "Footnote Crosswalk.csv" "footnote_crosswalk.csv"
 ## Remove headers from files  
 $ sed -i 1d *.csv  
 ## Load crucial tables into HDFS
-$ hdfs dfs -mkdir /user/w205/hospital_compare 
-$ hdfs dfs -put hospitals.csv /user/w205/hospital_compare  
-$ hdfs dfs -put effective_care.csv /user/w205/hospital_compare  
-$ hdfs dfs -put measures.csv /user/w205/hospital_compare  
-$ hdfs dfs -put readmissions.csv /user/w205/hospital_compare  
-$ hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
-$ hdfs dfs -put footnote_crosswalk.csv /user/w205/hospital_compare
+$ hdfs dfs -mkdir /user/w205/hospital_compare
+$ hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+$ hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+$ hdfs dfs -mkdir /user/w205/hospital_compare/measures
+$ hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+$ hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+$ hdfs dfs -mkdir /user/w205/hospital_compare/footnote_crosswalk
+$ hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals  
+$ hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care  
+$ hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
+$ hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
+$ hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
+$ hdfs dfs -put footnote_crosswalk.csv /user/w205/hospital_compare/footnote_crosswalk
