4fa7677668a34182d6d33750c13bd958e4dde442
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-24 23:17:48 -0700

    Update hive_base_ddl.sql

--- loading_and_modeling/hive_base_ddl.sql
+++ loading_and_modeling/hive_base_ddl.sql
@@ -10,6 +10,7 @@ CREATE EXTERNAL TABLE hospitals(pid VARCHAR(8),
                                 hospital_type VARCHAR(38), 
                                 hospital_own VARCHAR(45), 
                                 emergency_services VARCHAR(5))
+COMMENT 'Hospitals table containing details for a specific hospital key (pid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
@@ -21,11 +22,22 @@ LOCATION "/user/w205/hospital_compare";
 
 DROP TABLE effective_care;
 CREATE EXTERNAL TABLE effective_care(pid VARCHAR(8), 
+                                     hospital_name VARCHAR(52), 
+                                     address VARCHAR(52), 
+                                     city VARCHAR(22), 
+                                     state VARCHAR(4), 
+                                     zip VARCHAR(7), 
+                                     county VARCHAR(22), 
+                                     phone VARCHAR(12), 
                                      condition VARCHAR(37), 
                                      mid VARCHAR(20), 
+                                     measure_name VARCHAR(159), 
                                      score VARCHAR(44), 
                                      sample VARCHAR(15), 
-                                     fid VARCHAR(6))
+                                     fid VARCHAR(2), 
+                                     measure_start_date VARCHAR(21), 
+                                     measure_end_date VARCHAR(21))
+COMMENT 'Table contains measures (scores) of specific care (mid) for a particular hospital (pid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
@@ -37,13 +49,24 @@ LOCATION "/user/w205/hospital_compare";
 
 DROP TABLE readmissions;
 CREATE EXTERNAL TABLE readmissions(pid VARCHAR(8), 
+                                   hospital_name VARCHAR(52), 
+                                   address VARCHAR(52), 
+                                   city VARCHAR(22), 
+                                   state VARCHAR(4), 
+                                   zip VARCHAR(7), 
+                                   county VARCHAR(22), 
+                                   phone VARCHAR(12), 
+                                   measure_name VARCHAR(159), 
                                    mid VARCHAR(20), 
                                    national_comp VARCHAR(37), 
                                    sample VARCHAR(15), 
                                    score VARCHAR(15), 
                                    low_est VARCHAR(15), 
                                    high_est VARCHAR(15), 
-                                   fid VARCHAR(6))
+                                   fid VARCHAR(2), 
+                                   measure_start_date VARCHAR(21), 
+                                   measure_end_date VARCHAR(21))
+COMMENT 'Table contains measures (scores) of 30-day unplanned readmissions (mid) for a particular hospital (pid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
@@ -54,12 +77,13 @@ STORED AS TEXTFILE
 LOCATION "/user/w205/hospital_compare";
 
 DROP TABLE measures;
-CREATE EXTERNAL TABLE measures(mid VARCHAR(20), 
-                               measure_name VARCHAR(159), 
+CREATE EXTERNAL TABLE measures(measure_name VARCHAR(159), 
+                               mid VARCHAR(20), 
                                measure_start_quart VARCHAR(8), 
                                measure_start_date VARCHAR(21), 
                                measure_end_quart VARCHAR(8), 
                                measure_end_date VARCHAR(21))
+COMMENT 'Table contains definitions for the measures key (mid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
@@ -71,16 +95,39 @@ LOCATION "/user/w205/hospital_compare";
 
 DROP TABLE survey_responses;
 CREATE EXTERNAL TABLE survey_responses(pid VARCHAR(8), 
+                                       hospital_name VARCHAR(52), 
+                                       address VARCHAR(52), 
+                                       city VARCHAR(22), 
+                                       state VARCHAR(4), 
+                                       zip VARCHAR(7), 
+                                       county VARCHAR(22), 
+                                       nurses_comm_ap VARCHAR(15), 
+                                       nurses_comm_ip VARCHAR(15), 
                                        nurses_comm_ds VARCHAR(15), 
+                                       doctors_comm_ap VARCHAR(15), 
+                                       doctors_comm_ip VARCHAR(15), 
                                        doctors_comm_ds VARCHAR(15), 
+                                       staff_response_ap VARCHAR(15), 
+                                       staff_response_ip VARCHAR(15), 
                                        staff_response_ds VARCHAR(15), 
+                                       pain_mgmt_ap VARCHAR(15), 
+                                       pain_mgmt_ip VARCHAR(15), 
                                        pain_mgmt_ds VARCHAR(15), 
+                                       medicines_comm_ap VARCHAR(15), 
+                                       medicines_comm_ip VARCHAR(15), 
                                        medicines_comm_ds VARCHAR(15), 
+                                       clean_quiet_ap VARCHAR(15), 
+                                       clean_quiet_ip VARCHAR(15), 
                                        clean_quiet_ds VARCHAR(15), 
+                                       discharge_info_ap VARCHAR(15), 
+                                       discharge_info_ip VARCHAR(15), 
                                        discharge_info_ds VARCHAR(15), 
+                                       overall_rating_ap VARCHAR(15), 
+                                       overall_rating_ip VARCHAR(15), 
                                        overall_rating_ds VARCHAR(15), 
                                        hcahps_base_score VARCHAR(15), 
                                        hcahps_consist_score VARCHAR(15))
+COMMENT 'Table contains HCAHPS patient survey scores (hcahps_base_score) for a particular hospital (pid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
@@ -91,8 +138,9 @@ STORED AS TEXTFILE
 LOCATION "/user/w205/hospital_compare";
 
 DROP TABLE footnote_crosswalk;
-CREATE EXTERNAL TABLE footnote_crosswalk(fid VARCHAR(6), 
+CREATE EXTERNAL TABLE footnote_crosswalk(fid VARCHAR(2), 
                                          footnote VARCHAR(226))
+COMMENT 'Table definitions for the footnote key (fid)' 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
 "separatorChar"=",",
