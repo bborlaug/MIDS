9d70685c46caa96c13dfb125acc04383c131584e
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-27 23:06:55 -0700

    Update load_qs_data.sh

--- investigations/quality_score_calc/load_qs_data.sh
+++ investigations/quality_score_calc/load_qs_data.sh
@@ -1,6 +1,6 @@
-## Pulled in QS_table.csv produced in R (See code in /investigations/quality_score_calc)
-## cd to location of QS_table.csv
+## Pulled in QS_Table.csv produced in R (See code in /investigations/quality_score_calc)
+## cd to location of QS_Table.csv
 
-$ sed -i 1d QS_table1.csv
+$ sed -i 1d QS_Table.csv
 $ hdfs dfs -mkdir /user/w205/hospital_compare/quality_scores
-$ hdfs dfs -put QS_table1.csv /user/w205/hospital_compare/quality_scores
+$ hdfs dfs -put QS_Table.csv /user/w205/hospital_compare/quality_scores
