67b49373a74bc5365f86eca28e918ad510e60946
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-28 03:04:49 -0700

    Delete data_clean.py

--- transforming/data_clean.py
+++
@@ -1,8 +0,0 @@
-sqlContext.sql("USE hospital_compare")
-
-hospitals = sqlContext.sql("SELECT * FROM hospitals_tf")
-measures = sqlContext.sql("SELECT * FROM measures_tf")
-footnote_crosswalk = sqlContext.sql("SELECT * FROM footnote_crosswalk_tf")
-readmissions = sqlContext.sql("SELECT * FROM readmissions_tf")
-effective_care = sqlContext.sql("SELECT * FROM effective_care_tf")
-survey_responses = sqlContext.sql("SELECT * FROM survey_responses_tf")
