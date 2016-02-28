471e09b2a6d53cecd1ff7f9b33df062cac56ec86
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-02-27 21:30:05 -0700

    Create create_views.sql

---
+++ transforming/create_views.sql
@@ -0,0 +1,19 @@
+-- USE hospital_compare;
+
+-- Create a view that joins hospitals to their quality scores (for more info on computing/loading quality scores,
+-- see /investigations/quality_score_calc)
+
+CREATE VIEW hospital_quality AS
+  SELECT a.pid, a.hospital_name, a.city, a.state, b.QS
+  FROM hospitals_tf a
+  INNER JOIN quality_scores_tf b
+  ON a.pid = b.pid
+  WHERE QS<>'';
+  
+-- Create a view that aggregates hospital quality scores by their state & provides state summary statistics
+
+CREATE VIEW state_quality AS
+  SELECT state, COUNT(pid) AS count, AVG(QS) AS QS
+  FROM hospital_quality
+  GROUP BY state;
+
